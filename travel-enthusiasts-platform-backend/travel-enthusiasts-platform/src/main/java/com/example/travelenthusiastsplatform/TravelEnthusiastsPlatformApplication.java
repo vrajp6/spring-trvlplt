7fd340ce9144ddf4bf31d43093599a80ac7107c3
vrajp6 <vrajpatel2021@gmail.com>
2024-07-13 14:06:10 -0500

    feat: adding files

---
+++ travel-enthusiasts-platform-backend/travel-enthusiasts-platform/src/main/java/com/example/travelenthusiastsplatform/TravelEnthusiastsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelenthusiastsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelEnthusiastsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelEnthusiastsPlatformApplication.class, args);
+	}
+
+}
---
+++ travel-enthusiasts-platform-backend/travel-enthusiasts-platform/src/test/java/com/example/travelenthusiastsplatform/TravelEnthusiastsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelenthusiastsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelEnthusiastsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
